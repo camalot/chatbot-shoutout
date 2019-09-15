2bdcafbe138f0e85a47731da6839e4efe4f1e3bb
Ryan Conrad <camalot@gmail.com>
2019-09-14 22:10:25 -0500

    remove test script

--- test.sh
+++
@@ -1,8 +0,0 @@
-WORKSPACE=".";
-mkdir -p "${WORKSPACE}/temp/script";
-
-curl -sS $(curl -s https://api.github.com/repos/camalot/chatbotscriptupdater/releases/latest \
-| jq -r '.assets[0] .browser_download_url') > ${WORKSPACE}/temp/script/chatbotscriptupdater.zip
-mkdir -p ${WORKSPACE}/temp/script/libs/updater/
-unzip -d ${WORKSPACE}/temp/script/libs/updater/ ${WORKSPACE}/temp/script/chatbotscriptupdater.zip;
-rm "${WORKSPACE}/temp/script/chatbotscriptupdater.zip";
